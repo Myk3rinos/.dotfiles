1f945cc21e4d2be5b12b640f1b882d79ffe17239
Myk3rinos <fevre.william@wanadoo.fr>
2024-03-01 19:20:32 +0100

    Initial Commit
    install sway

--- configuration.nix
+++ configuration.nix
@@ -159,7 +159,7 @@
 #     wlr.enable = true;
 #     # gtk portal needed to make gtk apps happy
 #     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-};
+# };
 # hardware.wayland.compositor.enable = true;
 
 programs.sway = {
