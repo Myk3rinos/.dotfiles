3221d1be56e0e49ed12772fcbbea0aff6be3ce9c
Myk3rinos <fevre.william@wanadoo.fr>
2024-03-01 18:44:07 +0100

    Initial Commit
    install sway

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,8 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  wservices.xserver.wayland.enable = true;
+  services.xserver.wayland.enable = true;
+
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
@@ -137,6 +138,7 @@
     gnomeExtensions.clipboard-indicator
     gnomeExtensions.color-picker
 
+    wayland
     sway
     waybar
  ];
