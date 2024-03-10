ae2d921971784b1a29364cb7b532b645aa7aa28b
Myk3rinos <fevre.william@wanadoo.fr>
2024-03-10 12:31:59 +0100

    Initial Commit

--- script/update.sh
+++ script/update.sh
@@ -12,19 +12,19 @@ cpKeybinding() {
 cpFirefoxUser() {
   echo "Copying Firefox user"
   # cp -r ~/.mozilla/firefox/ ~/Documents/firefox/
-  cp -r ~/.mozilla/firefox/ /run/media/will/dd3/
-  cp ~/Documents/bookmarks.html /run/media/will/dd3/bookmarks.html
+  cp -r ~/.mozilla/firefox/ /run/media/$(user)/dd3/
+  cp ~/Documents/bookmarks.html /run/media/$(user)/dd3/bookmarks.html
 }
 
 cpDocument() {
   echo "Copying Documents"
-  cp -r ~/Images/ /run/media/will/dd3/
-  cp -r ~/Musique/ /run/media/will/dd3/
-  cp -r ~/Vidéos/ /run/media/will/dd3/
-  cp -r ~/.config/autostart/ /run/media/will/dd3/
-  cp -r ~/.config/gtk-4.0/ /run/media/will/dd3/
-  cp -r ~/.config/gh/ /run/media/will/dd3/
-  cp -r ~/.config/github-copilot/ /run/media/will/dd3/
+  cp -r ~/Images/ /run/media/$(user)/dd3/
+  cp -r ~/Musique/ /run/media/$(user)/dd3/
+  cp -r ~/Vidéos/ /run/media/$(user)/dd3/
+  cp -r ~/.config/autostart/ /run/media/$(user)/dd3/
+  cp -r ~/.config/gtk-4.0/ /run/media/$(user)/dd3/
+  cp -r ~/.config/gh/ /run/media/$(user)/dd3/
+  cp -r ~/.config/github-copilot/ /run/media/$(user)/dd3/
 }
 
 cpKeybinding
