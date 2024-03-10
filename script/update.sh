3499ec7ccc0a4311e2c6d0fd6739bf491b4e6e08
Myk3rinos <fevre.william@wanadoo.fr>
2024-03-10 16:14:43 +0100

    Initial Commit

--- script/update.sh
+++ script/update.sh
@@ -1,34 +1,33 @@
 #!/usr/bin/env bash
-#
 
 echo "------------- Update Start -------------"
 
 cpKeybinding() {
   echo "Copying custom keybindings to custom.txt"
-  # rm -f ~/.dotfiles/custom.txt
-  dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ > ~/.dotfiles/custom.txt
+  dconf dump /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ > /run/media/$USER/dd3/config/custom.txt
 }
 
 cpFirefoxUser() {
   echo "Copying Firefox user"
-  # cp -r ~/.mozilla/firefox/ ~/Documents/firefox/
-  cp -r ~/.mozilla/firefox/ /run/media/$(user)/dd3/
-  cp ~/Documents/bookmarks.html /run/media/$(user)/dd3/bookmarks.html
+  cp -r ~/.mozilla/firefox /run/media/$USER/dd3/config/
+  cp ~/Documents/bookmarks.html /run/media/$USER/dd3/config/bookmarks.html
 }
 
 cpDocument() {
   echo "Copying Documents"
-  cp -r ~/Images/ /run/media/$(user)/dd3/
-  cp -r ~/Musique/ /run/media/$(user)/dd3/
-  cp -r ~/Vidéos/ /run/media/$(user)/dd3/
-  cp -r ~/.config/autostart/ /run/media/$(user)/dd3/
-  cp -r ~/.config/gtk-4.0/ /run/media/$(user)/dd3/
-  cp -r ~/.config/gh/ /run/media/$(user)/dd3/
-  cp -r ~/.config/github-copilot/ /run/media/$(user)/dd3/
+  cp -r ~/Documents /run/media/$USER/dd3/config/
+  cp -r ~/Images /run/media/$USER/dd3/config/
+  cp -r ~/Musique /run/media/$USER/dd3/config/
+  cp -r ~/Vidéos /run/media/$USER/dd3/config/
+  cp -r ~/.config/autostart /run/media/$USER/dd3/config/
+  cp -r ~/.config/gtk-4.0 /run/media/$USER/dd3/config/
+  cp -r ~/.config/gh /run/media/$USER/dd3/config/
+  cp -r ~/.config/github-copilot /run/media/$USER/dd3/config/
+  cp -r ~/.dotfiles /run/media/$USER/dd3/config/
 }
 
 cpKeybinding
-# cpFirefoxUser
+cpFirefoxUser
 cpDocument
 
 echo "------------- Update Done -------------"
