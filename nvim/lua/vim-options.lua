83010a0cf2cfbdcefd2bc2198625f0efe6e27d83
Myk3rinos <fevre.william@wanadoo.fr>
2024-03-10 18:13:24 +0100

    Initial Commit

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,13 +4,16 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd("set number relativenumber")
-vim.cmd("se cursorline")
+-- vim.cmd("se cursorline")
+vim.cmd("set cursorline")
+vim.cmd("set clipboard+=unnamedplus")
 
--- vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>y", [["*y]])
 -- vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- vim.cmd(":Neotree filesystem reveal left")
 
---:set number
+---:set number--:set number-:set number
 --
 --:augroup numbertoggle
 --:  autocmd!
--- script/configure.sh
+++ script/configure.sh
@@ -8,7 +8,7 @@ echo "------------------ starting ------------------"
 
 setGnomeConfig() {
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-    gsettings set org.gnome.desktop.background picture-uri-dark file:///home/"$USER"/Images/wallpapers/2.jpg
+    gsettings set org.gnome.desktop.background picture-uri-dark file:///home/"$USER"/Images/wallpapers/9.jpg
 
     gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
     gsettings set org.gnome.desktop.peripherals.touchpad click-method 'fingers'
@@ -21,7 +21,7 @@ setGnomeConfig() {
     gsettings set org.gnome.mutter workspaces-only-on-primary false
     gsettings set org.gnome.mutter attach-modal-dialogs false
 
-    gsettings set org.gnome.desktop.interface gtk-theme "Nightfox-Dusk-BL-LB"
+    gsettings set org.gnome.desktop.interface gtk-theme 'Tokyonight-Dark-BL-LB"
     gsettings set org.gnome.desktop.interface icon-theme "Tokyonight-Dark"
     gsettings set org.gnome.desktop.interface cursor-theme "Adwaita"
     gsettings set org.gnome.desktop.interface monospace-font-name "Source Code Pro 10"
@@ -37,10 +37,10 @@ setGnomeConfig() {
     # gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 
     gsettings set org.gnome.shell.extensions.user-theme name "AlphaBlueNeon"
-    
-    gsettings set org.gnome.shell enabled-extensions "['dash-to-dock@micxgx.gmail.com', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'drive-menu@gnome-shell-extensions.gcampax.github.com', 'auto-move-windows@gnome-shell-extensions.gcampax.github.com', 'caffeine@patapon.info', 'clipboard-indicator@tudmotu.com', 'extension-list@tu.berry', 'color-picker@tuberry', 'top-bar-organizer@julian.gse.jsts.xyz', 'Vitals@CoreCoding.com', 'mediacontrols@cliffniff.github.com', 'desk-changer@eric.gach.gmail.com', 'tophat@fflewddur.github.io']"
+   
+    gsettings set org.gnome.shell enabled-extensions "['dash-to-dock@micxgx.gmail.com', 'user-theme@gnome-shell-extensions.gcampax.github.com', 'drive-menu@gnome-shell-extensions.gcampax.github.com', 'caffeine@patapon.info', 'clipboard-indicator@tudmotu.com', 'extension-list@tu.berry', 'color-picker@tuberry', 'mediacontrols@cliffniff.github.com', 'desk-changer@eric.gach.gmail.com', 'top-bar-organizer@julian.gse.jsts.xyz', 'pico-system-monitor@hiddewie', 'tophat@fflewddur.github.io', 'addshutbutton@flioner@jerom@olika.ovh', 'xremap@k0kubun.com', 'native-window-placement@gnome-shell-extensions.gcampax.github.com', 'auto-move-windows@gnome-shell-extensions.gcampax.github.com']"
 
-    gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Geary.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Music.desktop', 'org.gnome.Rhythmbox3.desktop', 'kitty.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Extensions.desktop']"
+    gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Geary.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Rhythmbox3.desktop', 'kitty.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Settings.desktop', 'org.gnome.Extensions.desktop']"
     # gsettings set org.gnome.shell.extensions.dash-to-dock extend-height 
     echo "------------------ gnome config set ------------------"
 
